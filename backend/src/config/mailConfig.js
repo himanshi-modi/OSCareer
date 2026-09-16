2ae54f387e9a2eb4be2c01a2157e5e0c058e0e3a
Himanshi Modi <himanshimodi421@gmail.com>
2026-09-17 00:32:47 +0530

    Updated mailconfig

--- backend/src/config/mailConfig.js
+++ backend/src/config/mailConfig.js
@@ -1,35 +1,12 @@
-const dns = require("dns");
-const nodemailer = require("nodemailer");
-
-dns.setDefaultResultOrder("ipv4first");
-
+const nodemailer=require("nodemailer");
 console.log("EMAIL_USER:", process.env.EMAIL_USER);
-
-console.log(
-  "EMAIL_PASSWORD:",
-  process.env.EMAIL_PASSWORD ? "Loaded" : "Missing"
-);
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
-  },
+console.log("EMAIL_PASSWORD:", process.env.EMAIL_PASSWORD ? "Loaded ✅" : "Missing ❌");
+const transporter=nodemailer.createTransport({
+    service:"gmail",
+    auth:{
+        user:process.env.EMAIL_USER,
+        pass:process.env.EMAIL_PASSWORD
+    }
 });
-transporter.verify((error, success) => {
-  if (error) {
-    console.error("❌ SMTP verification failed:", error);
-  } else {
-    console.log("✅ SMTP server is ready");
-  }
-});
-
 
 module.exports = transporter;
