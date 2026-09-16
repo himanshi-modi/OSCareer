4c0f8d7f5a48b28ad605ebe2bb5f079064e81861
Himanshi Modi <himanshimodi421@gmail.com>
2026-09-16 23:50:01 +0530

    Changed mailConfig.js

--- backend/src/config/mailConfig.js
+++ backend/src/config/mailConfig.js
@@ -5,7 +5,9 @@ const transporter=nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
-    service:"gmail",
+    connectionTimeout: 10000,
+  greetingTimeout: 10000,
+    
     auth:{
         user:process.env.EMAIL_USER,
         pass:process.env.EMAIL_PASSWORD
