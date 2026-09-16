8e7037ca5295a12841a6b8f140a99cbb92a6013c
Himanshi Modi <himanshimodi421@gmail.com>
2026-09-17 00:31:53 +0530

    Debug Email server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
 const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
+dns.lookup("smtp.gmail.com", { all: true }, (err, addresses) => {
+    if (err) {
+        console.error("GMAIL DNS ERROR:", err);
+        return;
+    }
 
+    console.log("GMAIL DNS ADDRESSES:", addresses);
+});
 
 const connectDB=require("./config/db");
 const app=require("./app");
