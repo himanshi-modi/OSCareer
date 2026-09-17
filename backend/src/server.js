ad628794de4334a9bb326ba7b982cd095ca8ee6d
Himanshi Modi <himanshimodi421@gmail.com>
2026-09-17 23:30:02 +0530

    Remove old Gmail DNS diagnostic

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,7 @@
 require("dotenv").config();
 const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
-dns.setDefaultResultOrder("ipv4first");
-dns.lookup("smtp.gmail.com", { all: true }, (err, addresses) => {
-    if (err) {
-        console.error("GMAIL DNS ERROR:", err);
-        return;
-    }
 
-    console.log("GMAIL DNS ADDRESSES:", addresses);
-});
 
 const connectDB=require("./config/db");
 const app=require("./app");
