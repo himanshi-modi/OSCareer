21e8de280af45309bccad29ead47c70371d32909
Himanshi Modi <himanshimodi421@gmail.com>
2026-09-17 11:22:53 +0530

    Updated mailConfig

--- backend/src/config/mailConfig.js
+++ backend/src/config/mailConfig.js
@@ -1,12 +1,15 @@
 const nodemailer=require("nodemailer");
 console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASSWORD:", process.env.EMAIL_PASSWORD ? "Loaded ✅" : "Missing ❌");
-const transporter=nodemailer.createTransport({
-    service:"gmail",
-    auth:{
-        user:process.env.EMAIL_USER,
-        pass:process.env.EMAIL_PASSWORD
-    }
+console.log("EMAIL_PASSWORD:", process.env.EMAIL_PASSWORD ? "Loaded " : "Missing ");
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD
+  }
 });
 
 module.exports = transporter;
